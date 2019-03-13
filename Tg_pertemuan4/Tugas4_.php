f81420a1264143ef0bc3c2e7821d90ac80e8e330
Syahrulz <47770251+Syahrulz@users.noreply.github.com>
2019-03-13 15:46:24 +0700

    Add files via upload

---
+++ Tg_pertemuan4/Tugas4_.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+		
+			<title>Tugas4</title>
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+		
+		<style type="text/css">
+			*{
+				margin: 0;
+			}
+		</style>
+		
+		</head>
+		
+		
+		<body>
+		
+		<div class="col-md-12" style="background-color: red; height: 50px; margin-bottom: 20px;">
+			<div class="col-md-6" style="background-color: white; margin-top:10px">
+				<form action="search_video.php" method="POST">
+					<input class="form-control ds-input" type="search" placeholder="Search.." name="title_video" style="width: 80%; float: left;">
+			        <input class="btn btn-outline-light" type="submit" value="Submit" style="float: left;">
+				</form>
+			</div>
+		</div>
+
+		<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "pemweb5maret";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$title = $_POST['title'];
+		$sql = "SELECT * FROM video_list WHERE title LIKE '%$title%'";
+		$result = $conn->query($sql);$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				?>
+
+		<div class="col-md-12" style="background-color: #ecf0f1; height: 200px; padding: 5px;">
+			
+			<div class="col-md-12" style="height:480px; background-color:grey">
+				<img src="<?php echo $row['thumbnail'] ?>" style="height: 470px; width: 100%">
+			</div>
+			
+			<div class="col-md-12" style="height:180px; background-color:white">
+					<div class="col-md-12" style="margin:2px;">
+						<a href="show_video.php?id_video=<php echo $row['id_video'] ?>">
+						<h4><?php echo $row['title']; ?></h4>
+							<p><?php echo $row['description'] ?></p>
+					</div>
+			</div>
+		
+		</div>
+
+		<?php
+		}
+		} else {
+			echo "0 results";
+		}
+
+		$conn->close();
+		?> 
+
+		</body>
+	
+	</html>
